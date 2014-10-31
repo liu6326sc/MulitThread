cc775dd3f8501807120ad8fd619b4e81ebfb23a8
liu6326sc <Ltian@Ltian-2.local>
2014-10-31 13:52:55 +0800

    Initial Commit

---
+++ MulitThread/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  MulitThread
+//
+//  Created by Ltian on 14/10/31.
+//  Copyright (c) 2014年 Ltian. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
